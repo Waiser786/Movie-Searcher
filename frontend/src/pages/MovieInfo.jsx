8c563f5ddeca6565740e24eea25daa35bc9ce5fb
Waleed-Dilresh <muhammed.abdoulcader@umail.uom.ac.mu>
2025-03-05 19:23:05 +0400

    Enabled dynamic navigation to movie details page when clicking on a selected movie

--- frontend/src/pages/MovieInfo.jsx
+++ frontend/src/pages/MovieInfo.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "../css/MovieInfo.css";
 import { useParams } from "react-router-dom";
 import { searchMovieByID } from "../services/api";
+import MovieCard from "../components/MovieCard";
 
 function MovieInfo() {
     const { id } = useParams();
@@ -23,15 +24,26 @@ function MovieInfo() {
         fetchMovie();
     }, [id]);
 
-    if (loading) return <h1>Loading...</h1>;
+    if (loading) return <p>Loading...</p>;
 
-    if (!movie) return <h1>Movie not found</h1>;
-
+    if (!movie) return <p>Movie not found</p>;
 
     return (
-        <div>
-            <h1>{movie.title}</h1>
-            <p>{movie.overview}</p>
+        <div className="Info-grid">
+            <div className="movie-info-card">
+                <MovieCard movie={movie} key={movie.id} />
+            </div>
+            <div>
+                <h2 className="movie-text ">{movie.title}</h2>
+                <p className="movie-text ">{movie.tagline}</p>
+                <p className="movie-text "><strong>Country of Origin: </strong>{movie.origin_country.join(", ")}</p>
+                <p className="movie-text "><strong>Genre: </strong>{movie.genres.map((genre) => genre.name).join(", ")}</p>
+                <p className="movie-text"><strong>Duration: </strong>{movie.runtime} minutes</p>
+                <p className="movie-text"><strong> Rate: </strong>{movie.vote_average}</p>
+                <p className="movie-text "><strong>Popularity: </strong>{movie.popularity}</p>
+                <h3 className="movie-text ">Overview</h3>
+                <p className="movie-text ">{movie.overview}</p>
+            </div>
         </div>
     );
 }
